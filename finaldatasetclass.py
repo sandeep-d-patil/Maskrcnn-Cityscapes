d17f042401494ac69d0c7395cc8a23df7aeaee96
sandeep <s.patil-1@student.tudelft.nl>
2020-06-10 03:12:07 +0200

    Was able to train and evaluate for one epoch.

--- finaldatasetclass.py
+++ finaldatasetclass.py
@@ -19,8 +19,8 @@ class CityscapesDataset(AbstractDataset):
         # load all image files, sorting them to
         # ensure that they are aligned
 
-        img_dir = "/media/sandeep/Data/cityscapes/datasets/cityscapes/leftImg8bit/"
-        ann_dir = "/media/sandeep/Data/cityscapes/datasets/cityscapes/gtFine_trainvaltest/gtFine/"
+        img_dir = "/home/sandeep/Downloads/cityscapes/leftImg8bit/"
+        ann_dir = "/home/sandeep/Downloads/cityscapes/gtFine/"
 
         img_dir = os.path.abspath(os.path.join(img_dir, split))
         img_pattern = os.path.join(img_dir, "*", "*_leftImg8bit.png")
@@ -45,9 +45,12 @@ class CityscapesDataset(AbstractDataset):
 
     def __getitem__(self, idx):
         # load images and masks
+        # idx = 287
         img_path = self.img_paths[idx]
         mask_path = self.ann_paths[idx]
         img = Image.open(img_path).convert("RGB")
+        # print("imag_path", img_path)
+        # print("idx", idx)
         # note that we haven't converted the mask to RGB,
         # because each color corresponds to a different instance
         # with 0 being background
@@ -56,77 +59,127 @@ class CityscapesDataset(AbstractDataset):
         ann_numpy = np.array(ann)  # ann numpy
         ann = torch.from_numpy(ann_numpy)  # ann torch
 
-        ### create labels
-        labels = []
-        instIds = torch.sort(torch.unique(ann))[0]
-        for instId in instIds:
-            if instId < 1000:  # group labels
-                continue
-
-            label = int(instId // 1000)
-            label = self.cityscapesID_to_ind[label]
-            labels.append(label)
-
-        ###create boxes (using ann numpy)
-
-        # instances are encoded as different colors
         obj_ids = np.unique(ann_numpy)
 
+        # print("obj_ids", obj_ids)
         # only keep relevant objects
         obj_ids = np.array([ids for ids in obj_ids if ids >= 1000])
+        # print("obj_id length", len(obj_ids))
+        # print("obj_id", obj_ids)
 
-        # split the color-encoded mask into a set
-        # of binary masks
-        masks = ann_numpy == obj_ids[:, None, None]
+        # if len(obj_ids) == 0:
+        #     empty_ann_path = self.get_img_info(idx)["ann_path"]
+        #     print("EMPTY ENTRY:", empty_ann_path)
+        #     self.img_paths.pop(idx)
+        #     self.ann_paths.pop(idx)
 
-        num_objs = len(obj_ids)
-        boxes = []
-        for i in range(num_objs):
-            pos = np.where(masks[i])
-            xmin = np.min(pos[1])
-            xmax = np.max(pos[1])
-            ymin = np.min(pos[0])
-            ymax = np.max(pos[0])
-            boxes.append([xmin, ymin, xmax, ymax])
+        while len(obj_ids) != 0:
+            ### create labels
+            labels = []
+            instIds = torch.sort(torch.unique(ann))[0]
+            for instId in instIds:
+                if instId < 1000:  # group labels
+                    continue
 
-        boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        # area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-        # convert to tensor
-        masks = torch.as_tensor(masks, dtype=torch.uint8)
-        labels = torch.as_tensor(labels, dtype=torch.int64)
+                label = int(instId // 1000)
+                label = self.cityscapesID_to_ind[label]
+                labels.append(label)
 
+            masks = ann_numpy == obj_ids[:, None, None]
+            # print(len(masks))
+
+            num_objs = len(obj_ids)
+            boxes = []
+            for i in range(num_objs):
+                pos = np.where(masks[i])
+                xmin = np.min(pos[1])
+                xmax = np.max(pos[1])
+                ymin = np.min(pos[0])
+                ymax = np.max(pos[0])
+                boxes.append([xmin, ymin, xmax, ymax])
+
+            boxes = torch.as_tensor(boxes, dtype=torch.float32)
+            # area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+            # convert to tensor
+            masks = torch.as_tensor(masks, dtype=torch.uint8)
+            labels = torch.as_tensor(labels, dtype=torch.int64)
+
+            image_id = torch.tensor([idx])
+
+            # print("labels shape", labels)
+            # print("masks shape", masks)
+            # print("image_id shape", image_id)
+            # print("boxes shape", boxes)
+
+            area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+
+            # suppose all instances are not crowd
+            iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+            target = {}
+            target["boxes"] = boxes
+            target["labels"] = labels
+            target["masks"] = masks
+            target["image_id"] = image_id
+            target["area"] = area
+            target["iscrowd"] = iscrowd
+
+            # print("labels shape", area.shape)
+            # print("masks shape", masks.shape)
+            # print("image_id shape", image_id.shape)
+            # print("boxes shape", boxes.shape)
+
+            if self.transforms is not None:
+                img, target = self.transforms(img, target)
+
+            return img, target
+
+        # masks = ann_numpy == obj_ids[:, None, None]
+        # print("obj_ids", obj_ids)
+
+        boxes = torch.tensor([[0.0, 0.0, 0.0, 0.0]], dtype=torch.float32)
+        # print("boxes shape", boxes.shape)
+        labels = torch.tensor([0], dtype=torch.int64)
+        masks = torch.zeros((1, 1024, 2048), dtype=torch.uint8)
+        # print("masks",masks.shape)
         image_id = torch.tensor([idx])
-
-        # area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
-        # suppose all instances are not crowd
-        # iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+        area = torch.tensor([[0.0]])
+        # num_objs = len(obj_ids)
+        iscrowd = torch.zeros(1, dtype=torch.int64)
 
         target = {}
         target["boxes"] = boxes
         target["labels"] = labels
         target["masks"] = masks
         target["image_id"] = image_id
-        # target["area"] = area
-        # target["iscrowd"] = iscrowd
-
-        # print("labels shape", area.shape)
-        # print("masks shape", masks.shape)
-        # print("image_id shape", image_id.shape)
-        # print("boxes shape", boxes.shape)
+        target["area"] = area
+        target["iscrowd"] = iscrowd
 
         if self.transforms is not None:
             img, target = self.transforms(img, target)
 
         return img, target
 
+
+
+
+
     def __len__(self):
         return len(self.img_paths)
 
     def get_img_info(self, index):
+
         # Reverse engineered from voc.py
         # All the images have the same size
-        return 0
+        return {
+            "height": 1024,
+            "width": 2048,
+            "idx": index,
+            "img_path": self.img_paths[index],
+            "ann_path": self.ann_paths[index],
+        }
 
 
-dataset = CityscapesDataset('/', split="train")
-dataset[0]
+# dataset = CityscapesDataset('/', split="val")
+# dataset[2]
+
--- maskrcnntrain.py
+++ maskrcnntrain.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torchvision
 import torch.utils.data
@@ -40,22 +41,26 @@ def get_instance_segmentation_model(num_classes):
 
 
 # use our dataset and defined transformations
-dataset = CityscapesDataset('/', "train",get_transform(train=False))
-dataset_test = CityscapesDataset('/',"test", get_transform(train=False))
+dataset = CityscapesDataset('/', "train", get_transform(train=False))
+dataset_test = CityscapesDataset('/', "val", get_transform(train=False))
 
 # split the dataset in train and test set
 torch.manual_seed(1)
-indices = torch.randperm(len(dataset)).tolist()
-dataset = torch.utils.data.Subset(dataset, indices[-50:])
-dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
+
+indices = np.arange(len(dataset)).tolist()
+dataset = torch.utils.data.Subset(dataset, indices[1:20])
+dataset_test = torch.utils.data.Subset(dataset_test, indices[1:20])
+# print(type(dataset_test))
+# dataset = dataset[0]
+# dataset_test = dataset_test[0]
 
 # define training and validation data loaders
 data_loader = torch.utils.data.DataLoader(
-    dataset, batch_size=2, shuffle=True, num_workers=4,
+    dataset, batch_size=2, shuffle=True, num_workers=0,
     collate_fn=utils.collate_fn)
 
 data_loader_test = torch.utils.data.DataLoader(
-    dataset_test, batch_size=2, shuffle=False, num_workers=4,
+    dataset_test, batch_size=2, shuffle=False, num_workers=0,
     collate_fn=utils.collate_fn)
 
 
@@ -82,7 +87,7 @@ lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
 
 
 # let's train it for 10 epochs
-num_epochs = 2
+num_epochs = 1
 
 for epoch in range(num_epochs):
     # train for one epoch, printing every 10 iterations
